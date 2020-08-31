b561acc3106bd5801807acbadd6e6623de5f98b7
illumination-k <illumination.k.27@gmail.com>
2020-09-01 00:40:04 +0900

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const info = require("../../package.json");
+
+const Footer = () => {
+  return <div>footer</div>;
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Menu from "./Menu";
+const info = require("../../package.json");
+
+const Header = () => {
+  return (
+    <div>
+      Header
+      <Menu />
+    </div>
+  );
+};
+
+export default Header;
