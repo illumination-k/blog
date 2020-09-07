506ed8634a1d7926b349021ac8cf85a2be7904ae
illumination-k <illumination.k.27@gmail.com>
2020-09-08 00:05:07 +0900

    refactor toAmpImage for url

--- libs/custom-loader/toAmpImg.js
+++ libs/custom-loader/toAmpImg.js
@@ -1,36 +1,55 @@
-const visit = require('unist-util-visit');
-const p = require('path')
-const sizeOf = require('image-size');
+const visit = require("unist-util-visit");
+const p = require("path");
+const sizeOf = require("image-size");
+
+// sync-requestを使わないと整形が終わったあとにリクエストされる。
+const sr = require("sync-request");
 
 module.exports = toAmpImg;
 
 function toAmpImg() {
-    return transformer
+  return transformer;
 
-    function transformer(ast) {
-        visit(ast, "image", visitor)
-        function visitor(node, index, parent) {
-            const url = node.url;
-            const alt = node.alt;
-            
-            let path = url;
-            if (url.startsWith("/")) {
-                path = p.join(process.cwd(), "public", url)
-            }
-            
-            const dimensions = sizeOf(path)
-            const height = dimensions.height;
-            const width = dimensions.width;
+  function transformNode(parent, index, url, alt, width, height, position) {
+    const value = `<amp-img layout="responsive" src="${url}" alt="${alt}" height="${height}" width="${width}" />`;
+    const newNode = {
+      type: "jsx",
+      value: value,
+      postion: position,
+    };
 
-            const position = node.postion;
-            const value = `<amp-img layout="responsive" src="${url}" alt="${alt}" height="${height}" width="${width}" />`
-            const newNode = {
-                type: "jsx",
-                value: value,
-                postion: position
-            }
+    parent.children[index] = newNode;
+  }
 
-            parent.children[index] = newNode;
-        }
+  function transformer(ast) {
+    visit(ast, "image", visitor);
+    function visitor(node, index, parent) {
+      const url = node.url;
+      const alt = node.alt;
+      const position = node.position;
+      let path = url;
+
+      if (url.startsWith("/")) {
+        path = p.join(process.cwd(), "public", url);
+        const dimensions = sizeOf(path);
+        const height = dimensions.height;
+        const width = dimensions.width;
+
+        transformNode(parent, index, url, alt, width, height, position);
+      } else if (url.startsWith("http") || url.startsWith("ftp")) {
+        const res = sr("GET", url);
+        const buf = Buffer.from(res.getBody());
+        const size = sizeOf(buf);
+        transformNode(
+          parent,
+          index,
+          url,
+          alt,
+          size.width,
+          size.height,
+          position
+        );
+      }
     }
+  }
 }
