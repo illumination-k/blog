3f7ad7a533365b81395aa9dc80d15ea6f5266935
illumination-k <illumination.k.27@gmail.com>
2020-09-30 03:30:19 +0900

    add nextofflineconfig

--- next.config.js
+++ next.config.js
@@ -37,8 +37,39 @@ const withMDX = require("@next/mdx")({
 
 const withOffline = require("next-offline");
 
+const nextOfflineConfig = {
+  workboxOpts: {
+    swDest: process.env.NEXT_EXPORT
+      ? "service-worker.js"
+      : "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "offlineCache",
+          expiration: {
+            maxEntries: 200,
+          },
+        },
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/service-worker.js",
+        destination: "/_next/static/service-worker.js",
+      },
+    ];
+  },
+};
+
 module.exports = withOffline(
-  withMDX({
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  })
+  withMDX(
+    {
+      pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    },
+    nextOfflineConfig
+  )
 );
