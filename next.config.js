0fa0b8ed8ec50b1dc2a564aeadd466e3d00b5ac3
illumination-k <illumination.k.27@gmail.com>
2020-09-30 03:23:01 +0900

    remove nextoffline config

--- next.config.js
+++ next.config.js
@@ -37,36 +37,7 @@ const withMDX = require("@next/mdx")({
 
 const withOffline = require("next-offline");
 
-const nextOfflineConfig = {
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? "service-worker.js"
-      : "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/service-worker.js",
-        destination: "/_next/static/service-worker.js",
-      },
-    ];
-  },
-};
-
 module.exports = withOffline(
-  nextOfflineConfig,
   withMDX({
     pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   })
