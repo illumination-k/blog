d7991c70cc63be559c4f1a56979427362336a4b4
illumination-k <illumination.k.27@gmail.com>
2020-09-30 03:52:28 +0900

    use next-pwa

--- next.config.js
+++ next.config.js
@@ -35,41 +35,17 @@ const withMDX = require("@next/mdx")({
   },
 });
 
-const withOffline = require("next-offline");
+const withPWA = require("next-pwa");
 
-const nextOfflineConfig = {
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? "service-worker.js"
-      : "static/service-worker.js",
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/service-worker.js",
-        destination: "/_next/static/service-worker.js",
-      },
-    ];
-  },
-};
-
-module.exports = withOffline(
+module.exports = withPWA(
   withMDX(
     {
       pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
     },
-    nextOfflineConfig
+    {
+      pwa: {
+        dest: "public",
+      },
+    }
   )
 );
