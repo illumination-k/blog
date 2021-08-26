4c11cfef66104c9fc457e1c8e9166c760d64b322
illumination-k <illumination.k.27@gmail.com>
2021-08-27 01:31:37 +0900

    update for closing illumination27.com

--- libs/custom-loader/highlighter.js
+++ libs/custom-loader/highlighter.js
@@ -46,10 +46,12 @@ refractor.register(require("refractor/lang/swift.js"));
 refractor.register(require("refractor/lang/sql.js"));
 refractor.register(require("refractor/lang/wasm.js"));
 refractor.register(require("refractor/lang/yaml.js"));
+refractor.register(require("refractor/lang/nim.js"));
 refractor.register(require("refractor/lang/powershell"));
 refractor.register(require("refractor/lang/json"));
 refractor.register(require("refractor/lang/graphql"));
 refractor.register(require("refractor/lang/toml"));
+
 
 function highlighter() {
   return (ast) => {
