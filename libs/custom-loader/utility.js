33ba544842278f8d2621959116f1d5fc7657777d
illumination-k <illumination.k.27@gmail.com>
2022-04-11 08:05:20 +0900

    remove unused loader

--- libs/custom-loader/utility.js
+++
@@ -1,9 +0,0 @@
-exports.transformToJsxNode = function (parent, index, value, position) {
-  const newNode = {
-    type: "jsx",
-    value: value,
-    postion: position,
-  };
-
-  parent.children[index] = newNode;
-};
