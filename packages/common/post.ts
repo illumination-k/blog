adf68462e75d9d40f1669970742510bfb3bf1637
illumination-k <illumination.k.27@gmail.com>
2022-11-08 03:26:28 +0900

    fmt

--- packages/common/post.ts
+++ packages/common/post.ts
@@ -1,11 +1,19 @@
 import { z } from "zod";
 
+const headingSchema = z.object({
+  depth: z.number(),
+  value: z.string(),
+});
+
+export type Heading = z.infer<typeof headingSchema>;
+
 export const postMetaSchema = z.object({
   id: z.string().uuid(),
   title: z.string(),
   description: z.string(),
   category: z.string(),
   tags: z.array(z.string()),
+  headings: z.array(headingSchema),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
